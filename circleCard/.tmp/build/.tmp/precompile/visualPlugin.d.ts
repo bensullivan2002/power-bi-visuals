a1e5aa8cb8dbd1ddf4749ab0f6472dea4e8d04ac
Ben Sullivan <bensullivan2002@gmail.com>
2022-05-03 22:39:15 +0100

    Change visual to a circle

---
+++ circleCard/.tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var circleCard3F6CF8CB79534DB895D386C6627141DA_DEBUG: IVisualPlugin;
+export default circleCard3F6CF8CB79534DB895D386C6627141DA_DEBUG;
---
+++ circleCard/.tmp/precompile/visualPlugin.ts
@@ -0,0 +1,32 @@
+import { Visual } from "../../src/visual";
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+import VisualConstructorOptions = powerbiVisualsApi.extensibility.visual.VisualConstructorOptions;
+import DialogConstructorOptions = powerbiVisualsApi.extensibility.visual.DialogConstructorOptions;
+var powerbiKey: any = "powerbi";
+var powerbi: any = window[powerbiKey];
+var circleCard3F6CF8CB79534DB895D386C6627141DA_DEBUG: IVisualPlugin = {
+    name: 'circleCard3F6CF8CB79534DB895D386C6627141DA_DEBUG',
+    displayName: 'CircleCard',
+    class: 'Visual',
+    apiVersion: '3.8.0',
+    create: (options: VisualConstructorOptions) => {
+        if (Visual) {
+            return new Visual(options);
+        }
+        throw 'Visual instance not found';
+    },
+    createModalDialog: (dialogId: string, options: DialogConstructorOptions, initialState: object) => {
+        const dialogRegistry = globalThis.dialogRegistry;
+        if (dialogId in dialogRegistry) {
+            new dialogRegistry[dialogId](options, initialState);
+        }
+    },
+    custom: true
+};
+if (typeof powerbi !== "undefined") {
+    powerbi.visuals = powerbi.visuals || {};
+    powerbi.visuals.plugins = powerbi.visuals.plugins || {};
+    powerbi.visuals.plugins["circleCard3F6CF8CB79534DB895D386C6627141DA_DEBUG"] = circleCard3F6CF8CB79534DB895D386C6627141DA_DEBUG;
+}
+export default circleCard3F6CF8CB79534DB895D386C6627141DA_DEBUG;
